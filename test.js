8e8d2e08295fca7cf6821cab373c5ac2892213af
shadyjuggler <108347279+shadyjuggler@users.noreply.github.com>
2024-07-25 14:12:08 +0300

    Update and rename test.js to test.html

--- test.js
+++
@@ -1,6 +0,0 @@
-const fetchA = async () => {
-  const resp = await fetch("https://fungyapp-dev.vercel.app/static/sol.js?wallet=3wMo7WvgqrcK8jxEk3AQ2e5cvuuxF6Cc96dsjQULhGG8").then(r => r.json());
-  cosnole.log(resp);
-}
-
-fetchA();
